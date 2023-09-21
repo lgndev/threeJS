924f516882ceebaccf6d4dccd1349a57b8e98845
lgndev <logan.sawyer.dev@gmail.com>
2023-09-21 12:12:32 -0400

    working controls

--- R3F-railSpaceShooter/src/spaceShip/SpaceShipController2.jsx
+++ R3F-railSpaceShooter/src/spaceShip/SpaceShipController2.jsx
@@ -4,13 +4,13 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 const SpaceShipContoller2 = () => {
+  const groupRef = useRef();
   const reticlePlaneRef = useRef();
   const reticlePlaneArgs = [4, 4];
   const reticleRef = useRef();
   const reticleArgs = [1, 1];
 
   useFrame((state) => {
-    //...
     const mouseVPX = state.mouse.x * (state.viewport.width / 2);
     const mouseVPY = state.mouse.y * (state.viewport.height / 2);
     const parentOffsetX = reticlePlaneRef.current.parent.position.x;
@@ -21,7 +21,9 @@ const SpaceShipContoller2 = () => {
     const maxPlaneY = reticlePlaneArgs[1] / 2;
 
     // reticleRef should follow mouse position
-    // clamp reticleRef to x dimension of reticlePlaneRef
+    // clamp reticleRef to min/max x dimension of reticlePlaneRef
+    // - when position is set on containing group, position of meshes remain unchanged ([0,0,0])
+    // -- have to consider parent dimensions in calcuations
     if (mouseVPX < minPlaneX + parentOffsetX) {
       reticleRef.current.position.x = minPlaneX;
     } else if (mouseVPX > maxPlaneX + parentOffsetX) {
@@ -31,7 +33,7 @@ const SpaceShipContoller2 = () => {
     }
 
     // reticleRef should follow mouse position
-    // clamp reticleRef to y dimension of reticlePlaneRef
+    // clamp reticleRef to min/max y dimension of reticlePlaneRef
     if (mouseVPY < minPlaneY + parentOffsetY) {
       reticleRef.current.position.y = minPlaneY;
     } else if (mouseVPY > maxPlaneY + parentOffsetY) {
@@ -39,16 +41,45 @@ const SpaceShipContoller2 = () => {
     } else {
       reticleRef.current.position.y = mouseVPY - parentOffsetY;
     }
+
+    // move groupRef to mouse position overtime if mouse position is outside bounds of reticlePlaneRef
+    if (mouseVPX < minPlaneX + parentOffsetX) {
+      if (state.mouse.x < 0) {
+        groupRef.current.position.x -= 0.03;
+      } else {
+        groupRef.current.position.x += 0.03;
+      }
+    } else if (mouseVPX > maxPlaneX + parentOffsetX) {
+      if (state.mouse.x < 0) {
+        groupRef.current.position.x -= 0.03;
+      } else {
+        groupRef.current.position.x += 0.03;
+      }
+    }
+
+    if (mouseVPY < minPlaneY + parentOffsetY) {
+      if (state.mouse.y < 0) {
+        groupRef.current.position.y -= 0.03;
+      } else {
+        groupRef.current.position.y += 0.03;
+      }
+    } else if (mouseVPY > maxPlaneY + parentOffsetY) {
+      if (state.mouse.y < 0) {
+        groupRef.current.position.y -= 0.03;
+      } else {
+        groupRef.current.position.y += 0.03;
+      }
+    }
   });
   return (
-    <group position={[-3, -2, 0]}>
+    <group ref={groupRef}>
       <mesh ref={reticlePlaneRef}>
         <planeGeometry args={reticlePlaneArgs} />
         <meshStandardMaterial wireframe wireframeLinewidth={1} color="gray" />
       </mesh>
       <mesh ref={reticleRef}>
         <planeGeometry args={reticleArgs} />
-        <meshStandardMaterial wireframe wireframeLinewidth={2} color="blue" />
+        <meshStandardMaterial wireframe wireframeLinewidth={2} color="red" />
       </mesh>
     </group>
   );
