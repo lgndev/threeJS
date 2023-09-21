ec27bd59a3c08dbb8898686b62281d3267bd92b3
lgndev <logan.sawyer.dev@gmail.com>
2023-09-21 14:29:56 -0400

    added new reticle behavior

--- R3F-railSpaceShooter/src/spaceShip/SpaceShipController2.jsx
+++ R3F-railSpaceShooter/src/spaceShip/SpaceShipController2.jsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 const SpaceShipContoller2 = () => {
+  const prevMouseState = useRef({ x: 0, y: 0 });
   const groupRef = useRef();
   const reticlePlaneRef = useRef();
   const reticlePlaneArgs = [4, 4];
@@ -24,23 +25,23 @@ const SpaceShipContoller2 = () => {
     // clamp reticleRef to min/max x dimension of reticlePlaneRef
     // - when position is set on containing group, position of meshes remain unchanged ([0,0,0])
     // -- have to consider parent dimensions in calcuations
-    if (mouseVPX < minPlaneX + parentOffsetX) {
-      reticleRef.current.position.x = minPlaneX;
-    } else if (mouseVPX > maxPlaneX + parentOffsetX) {
-      reticleRef.current.position.x = maxPlaneX;
-    } else {
-      reticleRef.current.position.x = mouseVPX - parentOffsetX;
-    }
+    // if (mouseVPX < minPlaneX + parentOffsetX) {
+    //   reticleRef.current.position.x = minPlaneX;
+    // } else if (mouseVPX > maxPlaneX + parentOffsetX) {
+    //   reticleRef.current.position.x = maxPlaneX;
+    // } else {
+    //   reticleRef.current.position.x = mouseVPX - parentOffsetX;
+    // }
 
     // reticleRef should follow mouse position
     // clamp reticleRef to min/max y dimension of reticlePlaneRef
-    if (mouseVPY < minPlaneY + parentOffsetY) {
-      reticleRef.current.position.y = minPlaneY;
-    } else if (mouseVPY > maxPlaneY + parentOffsetY) {
-      reticleRef.current.position.y = maxPlaneY;
-    } else {
-      reticleRef.current.position.y = mouseVPY - parentOffsetY;
-    }
+    // if (mouseVPY < minPlaneY + parentOffsetY) {
+    //   reticleRef.current.position.y = minPlaneY;
+    // } else if (mouseVPY > maxPlaneY + parentOffsetY) {
+    //   reticleRef.current.position.y = maxPlaneY;
+    // } else {
+    //   reticleRef.current.position.y = mouseVPY - parentOffsetY;
+    // }
 
     // move groupRef to mouse position overtime if mouse position is outside bounds of reticlePlaneRef
     // if (mouseVPX < minPlaneX + parentOffsetX) {
@@ -70,6 +71,73 @@ const SpaceShipContoller2 = () => {
     //     groupRef.current.position.y += 0.03;
     //   }
     // }
+
+    // reticleRef should move when the mouse moves
+    // - in the direction the mouse moves
+    // - clamp reticleRef to bounds of reticlePlaneRef
+    let mouseDelta = {
+      x: 0,
+      y: 0,
+    };
+    if (
+      prevMouseState.x !== state.mouse.x &&
+      prevMouseState.y !== state.mouse.y
+    ) {
+      mouseDelta = {
+        x: prevMouseState.current.x - state.mouse.x,
+        y: prevMouseState.current.y - state.mouse.y,
+      };
+    }
+
+    if (state.mouse.x <= -0.95) {
+      // edge case reticle transform
+      // - when mouse reaches edge of screen
+      if (minPlaneX < reticleRef.current.position.x) {
+        reticleRef.current.position.x -= 0.025;
+      }
+    } else if (state.mouse.x >= 0.95) {
+      // edge case reticle transform
+      // - when mouse reaches edge of screen
+      if (maxPlaneX > reticleRef.current.position.x) {
+        reticleRef.current.position.x += 0.025;
+      }
+    } else if (mouseDelta.x < 0) {
+      // standard reticle transform
+      if (maxPlaneX > reticleRef.current.position.x) {
+        reticleRef.current.position.x += 0.025;
+      }
+    } else if (mouseDelta.x > 0) {
+      // standard reticle transform
+      if (minPlaneX < reticleRef.current.position.x) {
+        reticleRef.current.position.x -= 0.025;
+      }
+    }
+
+    if (state.mouse.y <= -0.95) {
+      // edge case reticle transform
+      // - when mouse reaches edge of screen
+      if (minPlaneY < reticleRef.current.position.y) {
+        reticleRef.current.position.y -= 0.025;
+      }
+    } else if (state.mouse.y >= 0.95) {
+      // edge case reticle transform
+      // - when mouse reaches edge of screen
+      if (maxPlaneY > reticleRef.current.position.y) {
+        reticleRef.current.position.y += 0.025;
+      }
+    } else if (mouseDelta.y < 0) {
+      // standard reticle transform
+      if (maxPlaneY > reticleRef.current.position.y) {
+        reticleRef.current.position.y += 0.025;
+      }
+    } else if (mouseDelta.y > 0) {
+      // standard reticle transform
+      if (minPlaneY < reticleRef.current.position.y) {
+        reticleRef.current.position.y -= 0.025;
+      }
+    }
+    prevMouseState.current.x = state.mouse.x;
+    prevMouseState.current.y = state.mouse.y;
   });
   return (
     <group ref={groupRef}>
